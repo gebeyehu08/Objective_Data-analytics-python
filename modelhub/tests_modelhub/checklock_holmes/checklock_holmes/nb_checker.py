cbc1d125e44a9d44e0f2e2d4c2d7652e11b5908c
Kathia Barahona <katbara03@gmail.com>
2022-08-03 09:31:46 +0200

    use papermill engine and clients for nb execution

--- modelhub/tests_modelhub/checklock_holmes/checklock_holmes/nb_checker.py
+++ modelhub/tests_modelhub/checklock_holmes/checklock_holmes/nb_checker.py
@@ -1,45 +1,89 @@
 """
 Copyright 2022 Objectiv B.V.
 """
-import json
 import re
-import time
-from dataclasses import dataclass, field
-from functools import cached_property
-from typing import Any, Dict, List
+from dataclasses import dataclass
+
+import nbformat as nbformat
+from papermill.iorw import load_notebook_node
+from papermill.engines import NBClientEngine, NotebookExecutionManager
 
 from checklock_holmes.models.nb_checker_models import (
-    CellError, CellTiming, NoteBookCheck, NoteBookMetadata
+    NoteBookCheck, NoteBookMetadata, CellTiming, CellError
 )
 from checklock_holmes.settings import settings
 from checklock_holmes.utils.constants import (
     NB_SCRIPT_TO_STORE_TEMPLATE, SET_ENV_VARIABLE_TEMPLATE,
-    TIMING_CELL_CODE_TEMPLATE, WRAPPED_CODE_TEMPLATE
 )
 from checklock_holmes.utils.helpers import CuriousIncident
 from checklock_holmes.utils.supported_engines import SupportedEngine
 
-_DEFAULT_ENV_VARIABLES = {
-    'OBJECTIV_VERSION_CHECK_DISABLE': 'true'
-}
-_IGNORE_MARKDOWN_SQL_DISPLAYS = True
+
+class WatsonExecutionManager(NotebookExecutionManager):
+    MAX_LOG_EXCEPTION_MESSAGE = 500
+
+    def cell_exception(self, cell, cell_index=None, **kwargs) -> None:
+        super().cell_exception(cell, cell_index, **kwargs)
+
+        exc = kwargs.get('exception')
+
+        self.nb.metadata.papermill.error = CellError(
+            number=cell.metadata.papermill.index,
+            exc=f'{exc.ename}: {exc.evalue[:self.MAX_LOG_EXCEPTION_MESSAGE]}',
+        )
+
+    def notebook_start(self, **kwargs) -> None:
+        super().notebook_start(**kwargs)
+        for cell_idx, cell in enumerate(self.nb.cells):
+            cell.metadata.papermill.index = cell_idx
+
+
+class ChecklockEngine(NBClientEngine):
+    @classmethod
+    def execute_notebook(
+        cls,
+        nb,
+        kernel_name,
+        output_path=None,
+        progress_bar=True,
+        log_output=False,
+        autosave_cell_every=30,
+        **kwargs
+    ):
+        """
+        A wrapper to handle notebook execution tasks.
+
+        Wraps the notebook object in a `NotebookExecutionManager` in order to track
+        execution state in a uniform manner. This is meant to help simplify
+        engine implementations. This allows a developer to just focus on
+        iterating and executing the cell contents.
+        """
+        nb_man = WatsonExecutionManager(
+            nb,
+            output_path=None,  # don't save notebook outputs
+            progress_bar=False,  # ignore for now
+            autosave_cell_every=0,  # don't perform any autosave
+        )
+
+        nb_man.notebook_start()
+        try:
+            cls.execute_managed_notebook(nb_man, kernel_name, log_output=log_output, **kwargs)
+        finally:
+            nb_man.cleanup_pbar()
+            nb_man.notebook_complete()
+
+        return nb_man.nb
 
 
 @dataclass
 class NoteBookChecker:
-    MAX_LOG_EXCEPTION_MESSAGE = 500
-
     metadata: NoteBookMetadata
     display_cell_timing: bool
 
-    _errors: Dict[SupportedEngine, CellError] = field(default_factory=dict, init=False)
-    _cell_timings: List[CellTiming] = field(default_factory=list, init=False)
-
-    @cached_property
-    def cells(self) -> List[Dict[str, Any]]:
-        with open(self.metadata.path) as nb_file:
-            nb_data = json.load(nb_file)
-            return nb_data.get('cells')
+    _DEFAULT_ENV_VARIABLES = {
+        'OBJECTIV_VERSION_CHECK_DISABLE': 'true'
+    }
+    _ENV_VAR_CELL_TAG = 'injected-engine-variables'
 
     def check_notebook(self, engine: SupportedEngine) -> NoteBookCheck:
         """
@@ -55,72 +99,45 @@ class NoteBookChecker:
                 skipped=True,
             )
 
-        self._cell_timings = []
-        wrapped_script = self.get_script(engine, is_execution=True)
-        completed = True
-
-        start_time = time.time()
         try:
-            exec(wrapped_script)
+            executed_nb = ChecklockEngine.execute_notebook(
+                nb=self._load_notebook_node(engine),
+                kernel_name='python3',
+                output_path=None,  # don't save notebook outputs
+                progress_bar=False,  # ignore for now
+                autosave_cell_every=0,  # don't perform any autosave
+            )
         except Exception as exc:
-            if not self._errors.get(engine):
-                raise CuriousIncident(notebook_name=self.metadata.name, exc=exc)
+            raise CuriousIncident(notebook_name=self.metadata.name, exc=exc)
 
-            completed = False
-        end_time = time.time()
+        failing_block = ''
+        cell_timings = []
+        for idx, cell in enumerate(executed_nb.cells):
+            # ignore env setup and markdown cells
+            if self._ENV_VAR_CELL_TAG in cell.metadata.tags or cell.cell_type != 'code':
+                continue
 
-        error = self._errors.get(engine)
+            cell_exec_data = cell.metadata.papermill
+            if cell_exec_data.exception is True:
+                failing_block = cell.source
+                break
+
+            cell_timings.append(
+                CellTiming(number=idx, time=cell_exec_data.duration)
+            )
+
+        notebook_exec_data = executed_nb.metadata.papermill
         return NoteBookCheck(
             metadata=self.metadata,
             engine=engine,
-            completed=completed,
-            error=error,
-            failing_block=''.join(self.cells[error.number]['source']) if error else None,
-            elapsed_time=end_time - start_time,
-            elapsed_time_per_cell=self._cell_timings,
+            completed=notebook_exec_data.exception is None,
+            error=notebook_exec_data.error,
+            failing_block=failing_block,
+            elapsed_time=notebook_exec_data.duration,
+            elapsed_time_per_cell=cell_timings,
         )
 
-    @staticmethod
-    def _time_wrapped_cel_code(cell_number: int, code: str) -> str:
-        """
-        Wraps the cell's code for tracking the elapsed time on execution.
-        """
-        return TIMING_CELL_CODE_TEMPLATE.format(
-            code_to_time=code,
-            timing_stmt=f'self._log_cell_timing({cell_number}, elapsed_time)'
-        )
-
-    def _log_wrapped_cell_code(self, cell_number: int, engine: str, source: List[str]) -> str:
-        """
-        Wraps the cell's code for tracking raised exceptions on execution.
-        """
-        code = WRAPPED_CODE_TEMPLATE.format(
-            code_to_wrap='    '.join(source),
-            error_log_stmt=f'self._log_error({cell_number}, \"{engine}\",  e)',
-        )
-        if self.display_cell_timing:
-            code = self._time_wrapped_cel_code(cell_number, code)
-
-        return code
-
-    def _log_error(self, cell_number: int, engine: SupportedEngine,  exc: Exception):
-        """
-        Logs raised exception when running the cell.
-        """
-        self._errors[engine] = CellError(
-            number=cell_number,
-            exc=f'{exc.__class__.__name__}: {str(exc)[:self.MAX_LOG_EXCEPTION_MESSAGE]}...'
-        )
-
-    def _log_cell_timing(self, cell_number: int, elapsed_time: int) -> None:
-        """
-        Logs the elapsed time for the cell's execution.
-        """
-        self._cell_timings.append(
-            CellTiming(number=cell_number, time=elapsed_time)
-        )
-
-    def get_script(self, engine: SupportedEngine, is_execution: bool = True) -> str:
+    def get_script(self, engine: SupportedEngine) -> str:
         """
         Extracts all code cells from the notebook and generates a script based on it.
         If is_execution is True, then code cells will be wrapped for error logging
@@ -130,34 +147,18 @@ class NoteBookChecker:
         executing the script.
         """
         formatted_blocks = []
-        for cell_num, cell_metadata in enumerate(self.cells):
-            if cell_metadata['cell_type'] != 'code':
+        nb_node = self._load_notebook_node(engine)
+        for cell in nb_node.cells:
+            if cell.cell_type != 'code':
                 continue
 
-            if is_execution:
-                source_without_comments_and_displays = [
-                    stmt for stmt in cell_metadata['source']
-                    if not (
-                        stmt.startswith('#')
-                        # avoid printing on console
-                        or (_IGNORE_MARKDOWN_SQL_DISPLAYS and 'display_sql_as_markdown' in stmt)
-                    )
-                ]
-
-                if not source_without_comments_and_displays:
-                    continue
-
-                formatted_block = self._log_wrapped_cell_code(
-                    cell_num, engine, source=source_without_comments_and_displays,
-                )
-
-            else:
-                formatted_block = f'    # CELL {cell_num}\n    ' + '    '.join(cell_metadata['source'])
-
+            formatted_block = (
+                f'    # CELL {cell.metadata.papermill.index}\n    ' + '    '.join(cell.source)
+            )
             formatted_blocks.append(formatted_block)
 
         nb_script = '\n\n'.join(formatted_blocks)
-        if not is_execution and self.metadata.name:
+        if self.metadata.name:
             # creates script for debugging
             # the template defines a function for the entire notebook
             # and adds a call to it in if __name__ == '__main__'
@@ -166,15 +167,14 @@ class NoteBookChecker:
                 script=nb_script.strip(),
             )
 
-        return f'{self._get_env_setup_block(engine)}\n\n{nb_script}'
+        return nb_script
 
-    @staticmethod
-    def _get_env_setup_block(engine: SupportedEngine) -> str:
+    def _get_env_setup_block(self, engine: SupportedEngine) -> str:
         """
         Returns the code block for setting env variables based on the engine to use.
         """
         env_variables = settings.get_env_variables(engine)
-        env_variables.update(_DEFAULT_ENV_VARIABLES)
+        env_variables.update(self._DEFAULT_ENV_VARIABLES)
         env_variables_stmt = '\n'.join(
             [
                 SET_ENV_VARIABLE_TEMPLATE.format(
@@ -184,3 +184,16 @@ class NoteBookChecker:
             ]
         )
         return f'# env variables setup\nimport os\n{env_variables_stmt}'
+
+    def _load_notebook_node(
+        self,
+        engine: SupportedEngine,
+    ) -> nbformat.NotebookNode:
+        nb_node = load_notebook_node(notebook_path=self.metadata.path)
+        nb_node.metadata['papermill']['error'] = None
+
+        env_cell = nbformat.v4.new_code_cell(source=self._get_env_setup_block(engine))
+        env_cell.metadata['tags'] = [self._ENV_VAR_CELL_TAG]
+        env_cell.metadata['papermill'] = {}
+        nb_node.cells = [env_cell] + nb_node.cells
+        return nb_node
