b385514aa4f565baec919a9d1131f3171f5b0628
sdirosa <surai@objectiv.io>
2022-08-11 16:39:39 +0200

    Simplify props definition of TrackedSelect, now that TrackedInputContext does most of the work

--- tracker/trackers/react/src/trackedElements/TrackedSelect.tsx
+++ tracker/trackers/react/src/trackedElements/TrackedSelect.tsx
@@ -6,20 +6,10 @@ import React from 'react';
 import { TrackedInputContext, TrackedInputContextProps } from '../trackedContexts/TrackedInputContext';
 
 /**
- * TrackedSelect has the same props of a TrackedInput, except:
- * - The default value for `Component` is set to `select`.
- * - The default value of `eventHandler` is set to `onChange`
- */
-export type TrackedSelectProps = Omit<TrackedInputContextProps, 'Component'> & {
-  /**
-   * Optional. Defaults to the `value` attribute.
-   */
-  id?: string;
-};
-
-/**
  * Generates a TrackedInputContext preconfigured with a <select> Element as Component.
  */
-export const TrackedSelect = React.forwardRef<HTMLInputElement, Omit<TrackedSelectProps, 'Component'>>((props, ref) => (
-  <TrackedInputContext {...props} Component={'select'} eventHandler={props.eventHandler ?? 'onChange'} ref={ref} />
-));
+export const TrackedSelect = React.forwardRef<HTMLSelectElement, Omit<TrackedInputContextProps, 'Component'>>(
+  (props, ref) => (
+    <TrackedInputContext {...props} Component={'select'} ref={ref} />
+  )
+);
