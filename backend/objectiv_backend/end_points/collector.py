94f942045e8bef2a3cb956134569191dc1c69979
mike <mike@objectiv.io>
2022-08-23 16:40:06 +0200

    - properly propagate anonymous_mode
    - remove default values, so we die if it's nog set

--- backend/objectiv_backend/end_points/collector.py
+++ backend/objectiv_backend/end_points/collector.py
@@ -58,7 +58,7 @@ def collect(anonymous_mode: bool = False) -> Response:
             break
 
     # Do all the enrichment steps that can only be done in this phase
-    add_enriched_contexts(events)
+    add_enriched_contexts(events, anonymous_mode=anonymous_mode)
 
     set_time_in_events(events, current_millis, transport_time)
 
@@ -149,18 +149,18 @@ def _get_collector_response(error_count: int,
     return get_json_response(status=200, msg=msg, anonymous_mode=anonymous_mode, client_session_id=client_session_id)
 
 
-def add_enriched_contexts(events: EventDataList):
+def add_enriched_contexts(events: EventDataList, anonymous_mode: bool):
     """
     Enrich the list of events
     """
 
-    add_cookie_id_contexts(events)
+    add_cookie_id_contexts(events, anonymous_mode=anonymous_mode)
     for event in events:
         add_http_context_to_event(event=event, request=flask.request)
         add_marketing_context_to_event(event=event)
 
 
-def add_cookie_id_contexts(events: EventDataList, anonymous_mode: bool = False):
+def add_cookie_id_contexts(events: EventDataList, anonymous_mode: bool):
     """
     Modify the given list of events: Add the CookieIdContext to each event, if cookies are enabled.
     """
