071cd02aa7a73583146e39c6f9d536ac4779d9f1
Tom Jansen <10804239+jansentom@users.noreply.github.com>
2022-08-25 13:48:33 +0200

    update docstring, restrict types

--- bach/bach/series/series_json.py
+++ bach/bach/series/series_json.py
@@ -208,10 +208,13 @@ class SeriesJson(Series):
         if is_bigquery(dialect):
             if source_dtype in ('json', 'string'):
                 return expression
+            elif source_dtype in ('dict', 'list'):
+                # BQ can convert most types into json format, and since we use string as the container
+                # in our BQ implementation, this is basically a cast.
+                # https://cloud.google.com/bigquery/docs/reference/standard-sql/json_functions#to_json_string
+                return Expression.construct(f'to_json_string({{}})', expression)
             else:
-                # BQ can convert most types into json format
-                # https://cloud.google.com/bigquery/docs/reference/standard-sql/json_functions#to_json
-                return Expression.construct(f'to_json({{}})', expression)
+                raise ValueError(f'cannot convert {source_dtype} to json')
         raise DatabaseNotSupportedException(dialect)
 
     def to_pandas_info(self) -> Optional['ToPandasInfo']:
