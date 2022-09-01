b5323cddf0204ad07562c383eca371f72a62b860
thijs <thijs@getobjectiv.com>
2022-09-01 12:20:13 +0200

    Remove deprecated function

--- bach/bach/series/series_datetime.py
+++ bach/bach/series/series_datetime.py
@@ -60,34 +60,6 @@ class DateTimeOperation:
     def __init__(self, series: 'SeriesAbstractDateTime'):
         self._series = series
 
-    def sql_format(self, format_str: str) -> SeriesString:
-        """
-        Allow formatting of this Series (to a string type).
-
-        :param format_str: The format to apply to the date/time column.
-            Currently, this uses Postgres' data format string syntax:
-            https://www.postgresql.org/docs/14/functions-formatting.html
-
-        .. warning::
-            This method is deprecated, we recommend using :meth:`SeriesAbstractDateTime.dt.strftime` instead.
-
-        .. code-block:: python
-
-            df['year'] = df.some_date_series.dt.sql_format('YYYY')  # return year
-            df['date'] = df.some_date_series.dt.sql_format('YYYYMMDD')  # return date
-
-        :returns: a SeriesString containing the formatted date.
-        """
-        warnings.warn(
-            'Call to deprecated method, we recommend to use SeriesAbstractDateTime.dt.strftime instead',
-            category=DeprecationWarning,
-        )
-
-        expression = Expression.construct('to_char({}, {})',
-                                          self._series, Expression.string_value(format_str))
-        str_series = self._series.copy_override_type(SeriesString).copy_override(expression=expression)
-        return str_series
-
     def strftime(self, format_str: str) -> SeriesString:
         """
         Allow formatting of this Series (to a string type).
