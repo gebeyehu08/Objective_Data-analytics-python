8a0762911353ea9c93d49431973c6df1eba1a5ad
Tom Jansen <10804239+jansentom@users.noreply.github.com>
2022-09-05 21:08:45 +0200

    - Make test slightly nicer

--- bach/tests/functional/bach/test_df_database_create.py
+++ bach/tests/functional/bach/test_df_database_create.py
@@ -41,9 +41,7 @@ def test_database_create_table(engine, unique_table_test_name: str):
     assert df_from_table.base_node != df.base_node
     assert len(df_from_table.base_node.references) == 0
     assert df_from_table.base_node.materialization == Materialization.SOURCE
-    new_sql = to_sql(dialect=dialect, model=df_from_table.base_node)
-    expected_sql_fragment = f''
-    assert expected_sql_fragment in new_sql
+    assert to_sql(dialect=dialect, model=df_from_table.base_node) == ''
 
     # Second test: try to write table, but with overwrite=False. We expect an error
     df['y'] = random_value
