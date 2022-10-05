0ca12e04a50cc0c759157ae2c9e2565cc5da69e8
sdirosa <surai@objectiv.io>
2022-10-05 15:50:45 +0200

    RootLocationContext

--- schema/validator/validator.test.js
+++ schema/validator/validator.test.js
@@ -11,4 +11,45 @@ describe('RootLocationContext', () => {
       id: 'test',
     }).success).toBe(true);
   })
+
+ it('should fail due to wrong `id` type', () => {
+    expect(RootLocationContext.safeParse({
+      _type: 'RootLocationContext',
+      id: 123,
+    }).success).toBe(false);
+  })
+
+ it('should fail due to wrong `_type` type', () => {
+    expect(RootLocationContext.safeParse({
+      _type: 123,
+      id: 'test',
+    }).success).toBe(false);
+  })
+
+ it('should fail due to an extra property', () => {
+    expect(RootLocationContext.safeParse({
+      _type: 'RootLocationContext',
+      id: 'test',
+      extra: 123,
+    }).success).toBe(false);
+  })
+
+ it('should fail due to missing `id`', () => {
+    expect(RootLocationContext.safeParse({
+      _type: 'RootLocationContext'
+    }).success).toBe(false);
+  })
+
+ it('should fail due to missing `_type`', () => {
+    expect(RootLocationContext.safeParse({
+      id: 'test',
+    }).success).toBe(false);
+  })
+
+ it('should fail due to mismatching _type', () => {
+    expect(RootLocationContext.safeParse({
+      _type: 'NotRootLocationContext',
+      id: 'test',
+    }).success).toBe(false);
+  })
 })
