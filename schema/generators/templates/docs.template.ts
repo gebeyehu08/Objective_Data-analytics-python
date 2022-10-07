cbfdb2a7261590419fa69cbecea6e4bbfb9bc96f
sdirosa <surai@objectiv.io>
2022-10-07 14:07:02 +0200

    Remove TODOs that have been implemented

--- schema/generators/templates/docs.template.ts
+++ schema/generators/templates/docs.template.ts
@@ -41,6 +41,7 @@ getEntityNames().forEach((entityName) => {
     // TODO implement writeMermaid in docsWriter, e.g. writeMermaid({chart, caption, links})
 
     docsWriter.writeH3('Properties');
+
     // TODO implement writeTable in docsWriter, e.g. writeTable({title, rows:[cell1, cell2, ...cellN]})
     getObjectKeys(entityProperties).forEach((entityPropertyName) => {
       const { type, description } = entityProperties[entityPropertyName];
@@ -50,8 +51,5 @@ getEntityNames().forEach((entityName) => {
     docsWriter.writeEndOfLine();
 
     docsWriter.writeLine(admonitionDescription);
-
-    // TODO we have some admonitions in the current docs that would be nice to keep
-    // TODO Think of how can we add those to the base_schema OR use a map here, e.g. {entityName: admonition_text}
   });
 });
