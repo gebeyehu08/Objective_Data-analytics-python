768c988595b3218f690ae049acde9cd5bfdc60f2
KathiaBarahona <katbara03@gmail.com>
2022-10-13 14:16:39 +0200

    enable test

--- bach/tests/functional/bach/test_series_describe.py
+++ bach/tests/functional/bach/test_series_describe.py
@@ -28,7 +28,6 @@ def test_categorical_describe(engine) -> None:
     )
 
 
-@pytest.mark.skip_athena_todo()  # TODO: Athena  (does not support float as column name)
 def test_numerical_describe(engine) -> None:
     p_series = pd.Series(data=[1, 2, 3, 4, 5, 6, 7, 8, 1], name='numbers')
     series = DataFrame.from_pandas(engine=engine, df=p_series.to_frame(), convert_objects=True).numbers
