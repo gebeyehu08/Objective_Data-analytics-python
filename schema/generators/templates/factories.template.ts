3b502ff9ca6169d6b5041a0f27226bfb09282f3e
sdirosa <surai@objectiv.io>
2022-10-14 16:06:41 +0200

    ContextNames.ts

--- schema/generators/templates/factories.template.ts
+++ schema/generators/templates/factories.template.ts
@@ -17,7 +17,7 @@ import {
   getEventNames,
   getObjectKeys,
   getPropertyDescription,
-  getPropertyValue
+  getPropertyValue, sortArrayByName
 } from "./common";
 
 // TODO temporarily generate this in the /generated folder, as we need TS to be finished before we can use it
@@ -239,6 +239,66 @@ Generator.generateFromModel({ outputFile: `${destinationFolder}/EventFactories.t
   });
 
 });
+
+Generator.generateFromModel({ outputFile: `${destinationFolder}/ContextNames.ts` }, (writer: TextWriter) => {
+  const tsWriter = new TypescriptWriter(writer);
+  const globalContexts = contextNames.filter(contextName => {
+    const context = getEntityByName(contextName);
+    const parents = getEntityParents(context);
+    const isAbstract = contextName.startsWith('Abstract');
+    const isGlobalContext = parents.includes('AbstractGlobalContext');
+
+    return !isAbstract && isGlobalContext;
+  });
+
+  const locationContexts = contextNames.filter(contextName => {
+    const context = getEntityByName(contextName);
+    const parents = getEntityParents(context);
+    const isAbstract = contextName.startsWith('Abstract');
+    const isLocationContext = parents.includes('AbstractLocationContext');
+
+    return !isAbstract && isLocationContext;
+  })
+
+  // GlobalContextName enum
+  tsWriter.writeEnumeration({
+    export: true,
+    name: 'GlobalContextName',
+    members: sortArrayByName(globalContexts.map((_type) => ({ name: _type, value: _type }))),
+  });
+
+  tsWriter.writeEndOfLine();
+
+  // AnyGlobalContextName type
+  tsWriter.writeLine('export type AnyGlobalContextName =')
+  globalContexts.forEach((contextName, index) => {
+    tsWriter.writeLine(`${tsWriter.indentString}| '${contextName}'${index === globalContexts.length - 1 ? ';' : ''}`);
+  });
+
+  tsWriter.writeEndOfLine();
+
+  // LocationContextName enum
+  tsWriter.writeEnumeration({
+    export: true,
+    name: 'LocationContextName',
+    members: sortArrayByName(locationContexts.map((_type) => ({ name: _type, value: _type }))),
+  });
+
+  tsWriter.writeEndOfLine();
+
+  // AnyLocationContextName type
+  tsWriter.writeLine('export type AnyLocationContextName =')
+  locationContexts.forEach((contextName, index) => {
+    tsWriter.writeLine(`${tsWriter.indentString}| '${contextName}'${index === locationContexts.length - 1 ? ';' : ''}`);
+  });
+
+  tsWriter.writeEndOfLine();
+
+  tsWriter.writeLines([
+    'export const ContextNames = new Set([...Object.keys(LocationContextName), ...Object.keys(GlobalContextName)]);'
+  ]);
+});
+
 
 const getTypeForProperty = (property) => {
   const mappedType = SchemaToTypeScriptPropertyTypeMap[property.type];
