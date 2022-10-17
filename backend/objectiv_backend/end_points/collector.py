43c0d1fb59f42542e7a5471b615d5b42ef460da1
mike <mike@objectiv.io>
2022-10-17 15:30:49 +0200

    make sure to check for transport_time in event list, as we don't validate the event_list schema anymore

--- backend/objectiv_backend/end_points/collector.py
+++ backend/objectiv_backend/end_points/collector.py
@@ -120,6 +120,10 @@ def _get_event_data(request: Request) -> EventList:
         raise ValueError('Could not find events key in event_data')
     if not isinstance(event_data['events'], list):
         raise ValueError('events is not a list')
+    if 'transport_time' not in event_data:
+        raise ValueError('Could not find transport_time key in event_data')
+    if not isinstance(event_data['transport_time'], int):
+        raise ValueError('transport_time is not an int')
     if len(event_data['events']) > DATA_MAX_EVENT_COUNT:
         raise ValueError('Events exceeds limit')
 
