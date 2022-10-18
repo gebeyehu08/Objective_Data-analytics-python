11b2bb9a0627e749bc5198075102ac13a13cc5cb
sdirosa <surai@objectiv.io>
2022-10-18 22:19:27 +0200

    Use new parser in hierarchy generator

--- schema/generators/templates/hierarchy.template.ts
+++ schema/generators/templates/hierarchy.template.ts
@@ -5,18 +5,17 @@
 import { TextWriter } from '@yellicode/core';
 import { Generator } from '@yellicode/templating';
 import { JavaScriptWriter } from '../writers/JavaScriptWriter';
-import { getEntityByName, getEntityNames, getEntityParents } from './common';
+import { getContexts, getEvents } from "./parser";
 
 Generator.generate({ outputFile: `../../../backend/objectiv_backend/schema/hierarchy.json` }, (writer: TextWriter) => {
   const jsWriter = new JavaScriptWriter(writer, { writeCopyright: false });
 
   const hierarchy = {};
 
-  getEntityNames()
-    .sort()
-    .forEach((entityName) => {
-      const entityParents = getEntityParents(getEntityByName(entityName));
-      hierarchy[entityName] = [...entityParents, entityName];
+  [...getContexts(), ...getEvents()]
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .forEach((entity) => {
+      hierarchy[entity.name] = [...entity.parents.map(({ name }) => name), entity.name];
     });
 
   jsWriter.writeJSONObject(hierarchy);
