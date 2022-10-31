88de763c082529579e4ddc1d5ebc50fc8458c7f5
Ivar Pruijn <ivar@objectiv.io>
2022-10-31 16:10:23 +0100

    Prettify

--- schema/generators/templates/docs.template.ts
+++ schema/generators/templates/docs.template.ts
@@ -11,245 +11,258 @@ const destination = '../generated/docs/';
 let entitiesOverview = Array();
 
 export type PropertiesDefinition = {
-	name: string;
-	description: string;
-	type: string;
-	internal: boolean;
-	optional: boolean;
-	nullable: boolean;
-	items: {
-		type: string;
-	};
+  name: string;
+  description: string;
+  type: string;
+  internal: boolean;
+  optional: boolean;
+  nullable: boolean;
+  items: {
+    type: string;
+  };
 };
 
 [...getContexts(), ...getEvents()].forEach((entity) => {
-	const primaryDescription = entity.getDescription({ type: 'markdown', target: 'primary' });
-	const secondaryDescription = entity.getDescription({ type: 'markdown', target: 'secondary' });
-	const admonitionDescription = entity.getDescription({ type: 'markdown', target: 'admonition' });
-	
-	const isAbstract = entity.isAbstract;
-	const isLocationContext = entity.isLocationContext;
-	const isGlobalContext = entity.isGlobalContext;
-	
-	let frontMatterSlug = ''; // the documentation URL, specifically set for Abstracts
-	
-	let outputFile = (isLocationContext ? 'location-contexts/' : isGlobalContext ? 'global-contexts/' 
-	: 'events/') + entity.name + '.md';
-	if(isAbstract) {
-		// special case for AbstractContext; skip it
-		if (entity.name == 'AbstractContext') {
-			return;
-		}
-		outputFile = entity.name.replace('Abstract', '').replace(/[A-Z]/g, ' $&').trim().replace(' ', '-')
-		.toLowerCase() + 's/overview.md';
-		frontMatterSlug = "/taxonomy/" + outputFile.replace('overview.md', '');
+  const primaryDescription = entity.getDescription({ type: 'markdown', target: 'primary' });
+  const secondaryDescription = entity.getDescription({ type: 'markdown', target: 'secondary' });
+  const admonitionDescription = entity.getDescription({ type: 'markdown', target: 'admonition' });
 
-		// add this Abstract entity and its children to an Array used to generate the Reference overview page
-		let abstractEntity = {
-			name: entity.name.replace('Abstract', '').replace(/[A-Z]/g, ' $&').trim() + 's',
-			description: secondaryDescription,
-			listOfChildren: []
-		}
-		let listOfChildren = Array();
-		const children = entity.children;
-		if (children && children.length > 0) {
-			for (let i = 0; i < children.length; i++) {
-				const child = children[i];
-				const url = (child.isLocationContext ? './location-contexts/' 
-					: child.isGlobalContext ? './global-contexts/' : './events/') + child.name + '.md';
-				listOfChildren.push({
-					name: child.name,
-					url: url
-				})
-			}
-		}
-		abstractEntity.listOfChildren = listOfChildren;
-		entitiesOverview.push(abstractEntity);
-	}
+  const isAbstract = entity.isAbstract;
+  const isLocationContext = entity.isLocationContext;
+  const isGlobalContext = entity.isGlobalContext;
 
-	// extract required contexts for this entity
-	const rules = entity.ownRules;
-	let requiredContexts = Array();
-	if (rules && rules.length > 0) {
-		for (let i = 0; i < rules.length; i++) {
-			let rule = rules[i];
-			// show required contexts if they're location or global, and if it's either an AbstractEvent or 
-			// they contexts are not inherited
-			if (['RequiresLocationContext', 'RequiresGlobalContext'].includes(rule.type) 
-				&& (entity.name == 'AbstractEvent' || !rule._inheritedFrom)) {
-				const requiredName = rule.scope[0].context;
-				const type = rule.type.replace('Requires', '');
-				const url = '../' + type.replace('Context', '-contexts/').toLowerCase() + requiredName + '.md';
-				requiredContexts.push({
-					name: requiredName, 
-					type: type, 
-					url: url
-				})
-			}
-		}
-	}
+  let frontMatterSlug = ''; // the documentation URL, specifically set for Abstracts
 
-	Generator.generate({ outputFile: `${destination}/${outputFile}` }, (writer: TextWriter) => {
-		const docsWriter = new DocusaurusWriter(writer);
-		
-		docsWriter.writeFrontmatter(frontMatterSlug);
-		
-		docsWriter.writeH1(entity.name);
-		docsWriter.writeLine();
-		if(isAbstract) {
-			docsWriter.writeLine(secondaryDescription);
-		} 
-		else {
-			docsWriter.writeLine(primaryDescription);
-		}
-		docsWriter.writeLine();
+  let outputFile =
+    (isLocationContext ? 'location-contexts/' : isGlobalContext ? 'global-contexts/' : 'events/') + entity.name + '.md';
+  if (isAbstract) {
+    // special case for AbstractContext; skip it
+    if (entity.name == 'AbstractContext') {
+      return;
+    }
+    outputFile =
+      entity.name.replace('Abstract', '').replace(/[A-Z]/g, ' $&').trim().replace(' ', '-').toLowerCase() +
+      's/overview.md';
+    frontMatterSlug = '/taxonomy/' + outputFile.replace('overview.md', '');
 
-		docsWriter.writeLine("import Mermaid from '@theme/Mermaid'");
-		docsWriter.writeLine();
+    // add this Abstract entity and its children to an Array used to generate the Reference overview page
+    let abstractEntity = {
+      name: entity.name.replace('Abstract', '').replace(/[A-Z]/g, ' $&').trim() + 's',
+      description: secondaryDescription,
+      listOfChildren: [],
+    };
+    let listOfChildren = Array();
+    const children = entity.children;
+    if (children && children.length > 0) {
+      for (let i = 0; i < children.length; i++) {
+        const child = children[i];
+        const url =
+          (child.isLocationContext
+            ? './location-contexts/'
+            : child.isGlobalContext
+            ? './global-contexts/'
+            : './events/') +
+          child.name +
+          '.md';
+        listOfChildren.push({
+          name: child.name,
+          url: url,
+        });
+      }
+    }
+    abstractEntity.listOfChildren = listOfChildren;
+    entitiesOverview.push(abstractEntity);
+  }
 
-		// Mermaid chart
-		docsWriter.writeMermaidChartForEntity(entity, "Diagram: " + entity.name + ' inheritance');
-		docsWriter.writeLine();
+  // extract required contexts for this entity
+  const rules = entity.ownRules;
+  let requiredContexts = Array();
+  if (rules && rules.length > 0) {
+    for (let i = 0; i < rules.length; i++) {
+      let rule = rules[i];
+      // show required contexts if they're location or global, and if it's either an AbstractEvent or
+      // they contexts are not inherited
+      if (
+        ['RequiresLocationContext', 'RequiresGlobalContext'].includes(rule.type) &&
+        (entity.name == 'AbstractEvent' || !rule._inheritedFrom)
+      ) {
+        const requiredName = rule.scope[0].context;
+        const type = rule.type.replace('Requires', '');
+        const url = '../' + type.replace('Context', '-contexts/').toLowerCase() + requiredName + '.md';
+        requiredContexts.push({
+          name: requiredName,
+          type: type,
+          url: url,
+        });
+      }
+    }
+  }
 
-		// for Events: write list of required contexts
-		if (entity.isEvent) {
-			docsWriter.writeH3('Requires');
-			docsWriter.writeLine();
-			if (requiredContexts.length > 0) {
-				for (let i = 0; i < requiredContexts.length; i++) {
-					let rc = requiredContexts[i];
-					docsWriter.writeRequiredContext(rc.name, rc.url, rc.type);
-				}
-			} else {
-				docsWriter.writeLine('None.');
-			}
-			docsWriter.writeLine();
-		}
+  Generator.generate({ outputFile: `${destination}/${outputFile}` }, (writer: TextWriter) => {
+    const docsWriter = new DocusaurusWriter(writer);
 
-		/**
-		 * Create plain content rows from the given entity's properties.
-		 * @param properties The entity's properties.
-		 * @returns {Array<string[]>} - Rows of plain, formatted content.
-		 */
-		function getPropertiesRows(properties: Array<PropertiesDefinition>) {
-			let rows = Array() as [string[]];
-			properties.forEach((p) => {
-				const type = (p.type == "array") ? (p.type + "<" + p.items.type + ">") : p.type;
-				if(!p.internal) {
-					let name = '**' + p.name.replace(/_/g, '\\_') 
-						+ ((p.optional || p.nullable) ? ' _[optional]_' : '') + '**';
-					rows.push([name, type, p.description.replace(/(\r\n|\n|\r)/gm, "")]);
-				}
-			});
-			return rows;
-		}
-		
-		// table of own properties, if any
-		if (entity.ownProperties.length > 0) {
-			docsWriter.writeH3('Properties');
-			docsWriter.writeLine();
-			docsWriter.writeTable(['', 'type', 'description'], getPropertiesRows(entity.ownProperties));
-		}
+    docsWriter.writeFrontmatter(frontMatterSlug);
 
-		// table of inherited properties, if any
-		if (entity.inheritedProperties.length > 0) {
-			docsWriter.writeH3('Inherited Properties');
-			docsWriter.writeLine();
-			docsWriter.writeTable(['', 'type', 'description'], getPropertiesRows(entity.inheritedProperties));
-		}
+    docsWriter.writeH1(entity.name);
+    docsWriter.writeLine();
+    if (isAbstract) {
+      docsWriter.writeLine(secondaryDescription);
+    } else {
+      docsWriter.writeLine(primaryDescription);
+    }
+    docsWriter.writeLine();
 
-		docsWriter.writeEndOfLine();
-		
-		// if (entity.ownRules.length) {
-		//   docsWriter.writeH3('Validation Rules');
-		//   // Build a list of own rules summaries
-		//   let ruleSummaries = [];
-		//   entity.ownRules.forEach((entityRule) => {
-		//     getRuleSummaries(entityRule, ruleSummaries);
-		//   });
+    docsWriter.writeLine("import Mermaid from '@theme/Mermaid'");
+    docsWriter.writeLine();
 
-		//   // Remove dupes
-		//   [...new Set(ruleSummaries)]
-		//     // Sort
-		//     .sort((a, b) => a.localeCompare(b))
-		//     // Write a line per rule
-		//     .forEach((ruleSummary) => docsWriter.writeListItem(ruleSummary + "."));
+    // Mermaid chart
+    docsWriter.writeMermaidChartForEntity(entity, 'Diagram: ' + entity.name + ' inheritance');
+    docsWriter.writeLine();
 
-		//   docsWriter.writeEndOfLine();
-		// }
-		
-		// final notes
-		docsWriter.writeLine(admonitionDescription);
-	});
+    // for Events: write list of required contexts
+    if (entity.isEvent) {
+      docsWriter.writeH3('Requires');
+      docsWriter.writeLine();
+      if (requiredContexts.length > 0) {
+        for (let i = 0; i < requiredContexts.length; i++) {
+          let rc = requiredContexts[i];
+          docsWriter.writeRequiredContext(rc.name, rc.url, rc.type);
+        }
+      } else {
+        docsWriter.writeLine('None.');
+      }
+      docsWriter.writeLine();
+    }
+
+    /**
+     * Create plain content rows from the given entity's properties.
+     * @param properties The entity's properties.
+     * @returns {Array<string[]>} - Rows of plain, formatted content.
+     */
+    function getPropertiesRows(properties: Array<PropertiesDefinition>) {
+      let rows = Array() as [string[]];
+      properties.forEach((p) => {
+        const type = p.type == 'array' ? p.type + '<' + p.items.type + '>' : p.type;
+        if (!p.internal) {
+          let name = '**' + p.name.replace(/_/g, '\\_') + (p.optional || p.nullable ? ' _[optional]_' : '') + '**';
+          rows.push([name, type, p.description.replace(/(\r\n|\n|\r)/gm, '')]);
+        }
+      });
+      return rows;
+    }
+
+    // table of own properties, if any
+    if (entity.ownProperties.length > 0) {
+      docsWriter.writeH3('Properties');
+      docsWriter.writeLine();
+      docsWriter.writeTable(['', 'type', 'description'], getPropertiesRows(entity.ownProperties));
+    }
+
+    // table of inherited properties, if any
+    if (entity.inheritedProperties.length > 0) {
+      docsWriter.writeH3('Inherited Properties');
+      docsWriter.writeLine();
+      docsWriter.writeTable(['', 'type', 'description'], getPropertiesRows(entity.inheritedProperties));
+    }
+
+    docsWriter.writeEndOfLine();
+
+    // if (entity.ownRules.length) {
+    //   docsWriter.writeH3('Validation Rules');
+    //   // Build a list of own rules summaries
+    //   let ruleSummaries = [];
+    //   entity.ownRules.forEach((entityRule) => {
+    //     getRuleSummaries(entityRule, ruleSummaries);
+    //   });
+
+    //   // Remove dupes
+    //   [...new Set(ruleSummaries)]
+    //     // Sort
+    //     .sort((a, b) => a.localeCompare(b))
+    //     // Write a line per rule
+    //     .forEach((ruleSummary) => docsWriter.writeListItem(ruleSummary + "."));
+
+    //   docsWriter.writeEndOfLine();
+    // }
+
+    // final notes
+    docsWriter.writeLine(admonitionDescription);
+  });
 });
 
 // generate reference/overview.md for all the relevant Abstracts
 const outputFile = 'overview.md';
 const frontMatterSlug = '/taxonomy/reference/';
 Generator.generate({ outputFile: `${destination}/${outputFile}` }, (writer: TextWriter) => {
-	const docsWriter = new DocusaurusWriter(writer);
-		
-	docsWriter.writeFrontmatter(frontMatterSlug);
-	
-	docsWriter.writeH1("Open analytics taxonomy reference");
-	docsWriter.writeLine();
-	
-	entitiesOverview.forEach((category) => {
-		docsWriter.writeH2(category.name);
-		docsWriter.writeLine(category.description);
-		docsWriter.writeLine();
-		category.listOfChildren.forEach((child) => {
-			docsWriter.write('* ');
-			docsWriter.writeLink(child.name, child.url);
-			docsWriter.writeEndOfLine();
-		});
-		docsWriter.writeLine();
-	});
+  const docsWriter = new DocusaurusWriter(writer);
+
+  docsWriter.writeFrontmatter(frontMatterSlug);
+
+  docsWriter.writeH1('Open analytics taxonomy reference');
+  docsWriter.writeLine();
+
+  entitiesOverview.forEach((category) => {
+    docsWriter.writeH2(category.name);
+    docsWriter.writeLine(category.description);
+    docsWriter.writeLine();
+    category.listOfChildren.forEach((child) => {
+      docsWriter.write('* ');
+      docsWriter.writeLink(child.name, child.url);
+      docsWriter.writeEndOfLine();
+    });
+    docsWriter.writeLine();
+  });
 });
 
 const getRuleSummaries = (rule, ruleSummaries) => {
-	switch (rule.type) {
-		case 'MatchContextProperty':
-			rule.scope.forEach(({ contextA, contextB, property }) => {
-				ruleSummaries.push(`\`${contextA}.${property}\` should equal \`${contextB}.${property}\``);
-			});
-			break;
+  switch (rule.type) {
+    case 'MatchContextProperty':
+      rule.scope.forEach(({ contextA, contextB, property }) => {
+        ruleSummaries.push(`\`${contextA}.${property}\` should equal \`${contextB}.${property}\``);
+      });
+      break;
 
-		case 'RequiresLocationContext':
-			rule.scope.forEach(({ context, position }) => {
-				ruleSummaries.push(
-					`\`LocationStacks\` should contain \`${context}\`${position !== undefined ? ` at index ${position}` : ''}`
-				);
-			});
-			break;
+    case 'RequiresLocationContext':
+      rule.scope.forEach(({ context, position }) => {
+        ruleSummaries.push(
+          `\`LocationStacks\` should contain \`${context}\`${position !== undefined ? ` at index ${position}` : ''}`
+        );
+      });
+      break;
 
-		case 'RequiresGlobalContext':
-			rule.scope.forEach(({ context }) => {
-				ruleSummaries.push(`\`GlobalContexts\` should contain \`${context}\``);
-			});
-			break;
+    case 'RequiresGlobalContext':
+      rule.scope.forEach(({ context }) => {
+        ruleSummaries.push(`\`GlobalContexts\` should contain \`${context}\``);
+      });
+      break;
 
-		case 'UniqueContext':
-			rule.scope.forEach(({ excludeContexts, includeContexts, by }) => {
-				if (!excludeContexts?.length && !includeContexts?.length) {
-					ruleSummaries.push(`Items in \`${rule._inheritedFrom}\` should have a unique combination of \`{${by.join(', ')}}\` properties`);
-				}
-				if (excludeContexts?.length) {
-					ruleSummaries.push(
-						`Items in \`${rule._inheritedFrom}\` should have a unique combination of \`{${by.join(', ')}}\` properties, except for \`${excludeContexts.join(',')}\``
-					);
-				}
-				if (includeContexts?.length) {
-					includeContexts?.forEach((includedContext) => {
-						ruleSummaries.push(`${includedContext} should have a unique combination of \`{${by.join(', ')}}\` properties`);
-					});
-				}
-			});
-			break;
+    case 'UniqueContext':
+      rule.scope.forEach(({ excludeContexts, includeContexts, by }) => {
+        if (!excludeContexts?.length && !includeContexts?.length) {
+          ruleSummaries.push(
+            `Items in \`${rule._inheritedFrom}\` should have a unique combination of \`{${by.join(', ')}}\` properties`
+          );
+        }
+        if (excludeContexts?.length) {
+          ruleSummaries.push(
+            `Items in \`${rule._inheritedFrom}\` should have a unique combination of \`{${by.join(
+              ', '
+            )}}\` properties, except for \`${excludeContexts.join(',')}\``
+          );
+        }
+        if (includeContexts?.length) {
+          includeContexts?.forEach((includedContext) => {
+            ruleSummaries.push(
+              `${includedContext} should have a unique combination of \`{${by.join(', ')}}\` properties`
+            );
+          });
+        }
+      });
+      break;
 
-		default:
-			throw new Error(`Cannot summarize rule ${rule.type}`);
-	}
+    default:
+      throw new Error(`Cannot summarize rule ${rule.type}`);
+  }
 
-	return ruleSummaries;
+  return ruleSummaries;
 };
