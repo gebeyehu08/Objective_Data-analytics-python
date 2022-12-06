01e4297e727f1b433936afad6b52606c160f3202
thijs <thijs@objectiv.io>
2022-12-06 10:28:56 +0100

    Add dockerized demo setup for Bach only

---
+++ bach/create_test_data.sql
@@ -0,0 +1,20 @@
+CREATE TABLE example (
+        city_id bigint,
+        skating_order bigint,
+        city text,
+        municipality text,
+        inhabitants bigint,
+        founding bigint
+);
+insert into example(city_id, skating_order, city, municipality, inhabitants, founding) values
+(1,  1,  'Ljouwert',   'Leeuwarden',        93485, 1285),
+(2,  2,  'Snits',      'Súdwest-Fryslân',   33520, 1456),
+(3,  3,  'Drylts',     'Súdwest-Fryslân',   3055,  1268),
+(4,  4,  'Sleat',      'De Friese Meren',   700,   1426),
+(5,  5,  'Starum',     'Súdwest-Fryslân',   960,   1061),
+(6,  6,  'Hylpen',     'Súdwest-Fryslân',   870,   1225),
+(7,  7,  'Warkum',     'Súdwest-Fryslân',   4440,  1399),
+(8,  8,  'Boalsert',   'Súdwest-Fryslân',   10120, 1455),
+(9,  9,  'Harns',      'Harlingen',         14740, 1234),
+(10, 10, 'Frjentsjer', 'Waadhoeke',         12760, 1374),
+(11, 11, 'Dokkum',     'Noardeast-Fryslân', 12675, 1298);
